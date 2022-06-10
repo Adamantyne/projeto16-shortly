e5a57b597878aafe004ffc9bae040b75c23b8ff7
Guilherme Alves <guialvest@gmail.com>
2022-06-10 14:04:17 -0300

    Configurações de deploy

--- src/db.js
+++ src/db.js
@@ -1,15 +1,20 @@
+import pg from 'pg';
+
 import dotenv from "dotenv";
-import pg from "pg";
 
-const {Pool} = pg;
 dotenv.config();
+const { Pool } = pg;
 
-const db = new Pool({
-  user:process.env.DB_USER,
-  password:process.env.DB_PASSWORD,
-  host:process.env.HOST,
-  port:process.env.DB_PORT,
-  database:process.env.DATABASE
-});
+const databaseConfig = {
+    connectionString: process.env.DATABASE_URL,
+}
+
+if(process.env.MODE==="PROD"){
+    databaseConfig.ssl= {
+        rejectUnauthorized: false
+    }
+}
+
+const db = new Pool(databaseConfig);
 
 export default db;
